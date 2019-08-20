1d72b20811dcec640159b2879816618392c790ea
fchitivah <fchitivah@gmail,com>
2019-08-19 22:38:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/listexample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.listexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    // Array of strings...
+    ListView simpleList;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
+    }
+}
